b79e0e5bb15837e40450483faa828e199fe48c10
Albert <adalbert123640@gmail.com>
2022-10-26 19:15:31 +0200

    fix custom 505 error

--- niet/views.py
+++ niet/views.py
@@ -9,5 +9,5 @@ def permission_denied_view(request, exception):
 def page_not_found_view(request, exception):
     return render(request, '404.html', status=404)
 
-def server_error_view(exception):
-    return render('500.html', status=500)
+def server_error_view(request, *args, **argv):
+    return render(request, '500.html', status=500)
