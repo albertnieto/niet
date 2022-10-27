1517677cab0d4aa5bfb6bf1f6b89df3534bb5fd4
Albert <adalbert123640@gmail.com>
2022-10-27 15:50:01 +0200

    added custom user manager

--- niet/blog/admin.py
+++ niet/blog/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from niet.blog.models import Post
+from django.contrib.auth.models import Group as authGroup
+from niet.blog.models import Post, User, Group
 
+# Unregister Django auth group so we can use our custom group
+admin.site.unregister(authGroup)
+
+# Each class must be in a seperate line
+admin.site.register(User)
+admin.site.register(Group)
 admin.site.register(Post)
---
+++ niet/blog/managers.py
@@ -0,0 +1,31 @@
+"""
+A Manager is the interface through which database query operations are provided to Django models. 
+At least one Manager exists for every model in a Django application.
+"""
+from django.contrib.auth.base_user import BaseUserManager
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, password, email=None):
+        if username is None:
+            raise TypeError('Users must have an identifier.')
+
+        if password is None:
+            raise TypeError('Users must have a password.')
+
+        user = self.model(
+            username = username,
+            email=self.normalize_email(email)
+        )
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, username, password):
+        user = self.create_user(username, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
