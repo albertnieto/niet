0c7915fc0435a3d5741fcf0ee07700b27519b31a
Albert <adalbert123640@gmail.com>
2022-10-28 15:39:59 +0200

    fix post id generation

--- niet/utils/tokens.py
+++ niet/utils/tokens.py
@@ -1,5 +1,5 @@
 from secrets import token_urlsafe
 
 # Use of secrets libray to create an urlsafe ID
-def random_id_field(nbytes=11):
+def random_id_field(nbytes=7):
     return token_urlsafe(nbytes)
