b29c2aa97e970871fbe7434f9cc748d29a781d89
Albert <adalbert123640@gmail.com>
2022-11-07 23:06:03 +0100

    added ground and glft loader

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { useEffect } from 'react'
 import * as THREE from 'three'
+import { GLTFLoader  } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import './App.css'
 import InitialScene from './components/threejs/initialScene'
 
@@ -14,20 +15,47 @@ function App() {
     background.initialize();
     background.animate();
 
-    const boxGeometry = new THREE.BoxGeometry(16, 16, 16);
-    const boxMaterial = new THREE.MeshNormalMaterial();
-    const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+    //background.scene.background = new THREE.Color('0x000000');
+    background.scene.fog = new THREE.Fog('0x000000', 1, 100);
 
-    background.scene.add(boxMesh);
-  
+    const groundGeometry = new THREE.PlaneGeometry( 100, 100 );
+    const groundMaterial = new THREE.MeshPhysicalMaterial({
+      color:'0x000000'
+    });
+    const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
+    groundMesh.position.y = 0.0;
+    groundMesh.rotation.x = - Math.PI / 2;
+    groundMesh.receiveShadow = true;
+
+    const material = new THREE.MeshPhysicalMaterial({
+      color:'0x121212'
+    });
+
+    const glftLoader  = new GLTFLoader();
+    glftLoader.load('./src/assets/eixample_trim_small_decimated.glb', (gltfScene) => {
+      
+      gltfScene.scene.traverse( function ( child ) {
+        if ( child.isMesh ) {
+            child.material = material;
+            child.castShadow = true;
+            child.receiveShadow = false;
+            child.flatshading = true;
+            child.position.y = 0.0
+        }
+      });
+
+      background.scene.add(groundMesh);
+      background.scene.add(gltfScene.scene);
+
+    });
+
   }, []);
 
   return (
     <div>
-      <canvas id="background"/>
-      <div className="App">
+      <canvas id="background">
         <h1>Albert Nieto</h1>
-      </div>
+      </canvas>
     </div>
   )
 }
