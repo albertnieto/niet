1515db4d968931f1771cb7dcb5bb6b8dab6b6255
Albert <adalbert123640@gmail.com>
2022-11-08 01:37:19 +0100

    moved to new branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,39 +15,6 @@ function App() {
     background.initialize();
     background.animate();
 
-    //background.scene.background = new THREE.Color('0x000000');
-    background.scene.fog = new THREE.Fog('0x000000', 1, 100);
-
-    const groundGeometry = new THREE.PlaneGeometry( 100, 100 );
-    const groundMaterial = new THREE.MeshPhysicalMaterial({
-      color:'0x000000'
-    });
-    const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
-    groundMesh.position.y = 0.0;
-    groundMesh.rotation.x = - Math.PI / 2;
-    groundMesh.receiveShadow = true;
-
-    const material = new THREE.MeshPhysicalMaterial({
-      color:'0x121212'
-    });
-
-    const glftLoader  = new GLTFLoader();
-    glftLoader.load('./src/assets/eixample_trim_small_decimated.glb', (gltfScene) => {
-      
-      gltfScene.scene.traverse( function ( child ) {
-        if ( child.isMesh ) {
-            child.material = material;
-            child.castShadow = true;
-            child.receiveShadow = false;
-            child.flatshading = true;
-            child.position.y = 0.0
-        }
-      });
-
-      background.scene.add(groundMesh);
-      background.scene.add(gltfScene.scene);
-
-    });
 
   }, []);
 
