c03af6b07ed94d7e6c56979a3abe042dcb04222d
Albert <adalbert123640@gmail.com>
2022-11-11 16:45:10 +0100

    Create Html.jsx

---
+++ frontend/src/components/Html.jsx
@@ -0,0 +1,76 @@
+export default function Html() {
+    return (
+      <>
+        <h1
+        style={{
+          position: 'absolute',
+          top: '50vh',
+          left: '50vw',
+          transform: 'translateX(-50%)',
+          color: '#ee6644'
+        }}>
+          hello.
+        </h1>
+        <h1
+        style={{
+          position: 'absolute',
+          top: '100vh',
+          left: '50vw',
+          transform: 'translateX(-50%)',
+          color: '#ee6644'
+        }}>
+          hey.
+        </h1>
+        <h1
+        style={{
+          position: 'absolute',
+          top: '150vh',
+          left: '50vw',
+          transform: 'translateX(-50%)',
+          color: '#ee6644'
+        }}>
+          hi.
+        </h1>
+        <h1
+        style={{
+          position: 'absolute',
+          top: '200vh',
+          left: '50vw',
+          transform: 'translateX(-50%)',
+          color: '#ee6644'
+        }}>
+          7.
+        </h1>
+        <h1
+        style={{
+          position: 'absolute',
+          top: '250vh',
+          left: '50vw',
+          transform: 'translateX(-50%)',
+          color: '#ee6644'
+        }}>
+          8.
+        </h1>
+        <h1
+        style={{
+          position: 'absolute',
+          top: '300vh',
+          left: '50vw',
+          transform: 'translateX(-50%)',
+          color: '#ee6644'
+        }}>
+          9.
+        </h1>
+        <h1
+        style={{
+          position: 'absolute',
+          top: '350vh',
+          left: '50vw',
+          transform: 'translateX(-50%)',
+          color: '#ee6644'
+        }}>
+          10.
+        </h1>
+      </>
+    )
+  }
