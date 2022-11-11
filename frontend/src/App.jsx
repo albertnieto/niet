f0e4409defc1ddd121f89f64e4d572662b61ceab
Albert <adalbert123640@gmail.com>
2022-11-11 10:20:04 +0100

    added controls and shadows

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,30 @@ import * as THREE from "three";
 
 import React, { useEffect } from "react";
 import { Canvas, useThree, extend } from '@react-three/fiber'
-import { OrbitControls, PresentationControls } from '@react-three/drei'
+import { 
+  OrbitControls, 
+  PresentationControls,
+  PerspectiveCamera
+} from '@react-three/drei'
 
 import Office from './components/Office'
+import Office2 from './components/Office2'
 
 export default function App() {
   return (
-    <Canvas>
+    <Canvas shadows={true}>
+      <PerspectiveCamera position={[10, 1, 10]} />
       <OrbitControls />
       <color attach="background" args={['#121212']} />
-      <spotLight position={[5, 5, 2]} angle={0.15} penumbra={0.3} />
+      <spotLight 
+        position={[3, 3, 3]} 
+        angle={0.2} 
+        penumbra={0.7}
+        intensity={1.5}
+        castShadow
+        shadow-mapSize-height={512}
+        shadow-mapSize-width={512}
+      />
       <Office />
     </Canvas>
   )
