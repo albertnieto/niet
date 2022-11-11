359a3c00331fbb93e4ca992d45693dcb11ce05f2
Albert <adalbert123640@gmail.com>
2022-11-11 16:45:34 +0100

    add camera movements and html scrolls

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,37 @@
 import * as THREE from "three";
+
 import React, { 
   useEffect,
   useRef,
 } from "react";
+
 import { 
   Canvas, 
   useThree, 
   useFrame,
   extend, 
 } from '@react-three/fiber'
+
 import { 
   OrbitControls, 
   PresentationControls,
   PerspectiveCamera,
+  Scroll, 
+  ScrollControls,
 } from '@react-three/drei'
+
 import Office from './components/Office'
-
-const deg2rad = degrees => degrees * (Math.PI / 180);
-
-function Camera(props) {
-  const ref = useRef()
-  const set = useThree((state) => state.set);
-  // This makes sure that size-related calculations are proper
-  // Every call to useThree will return this camera instead of the default camera 
-  useEffect(() => void set({ camera: ref.current }), []);
-  useFrame(() => ref.current.updateMatrixWorld());
-  return <perspectiveCamera ref={ref} {...props} />
-}
+import Html from './components/Html'
+import {Camera, CameraMovements} from './components/Camera'
 
 export default function App() {
   return (
     <Canvas shadows={true}>
-      <OrbitControls />
+      <color attach="background" args={['#ffffff']} />
       <Camera 
-        position={[1, 3, 0]} 
-        rotation={[deg2rad(30), 0, 0]} 
+          position={[0.7, 2, 0]} 
+          rotation={[0, 0, 0]} 
       />
-      <color attach="background" args={['#121212']} />
       <spotLight 
         position={[1, 3, 0]} 
         angle={0.2} 
@@ -47,6 +42,17 @@ export default function App() {
         shadow-mapSize-width={512}
       />
       <Office />
+      <ScrollControls pages={7}>
+
+        <Scroll>
+          <CameraMovements />
+        </Scroll>
+
+        <Scroll html>
+          <Html />
+        </Scroll>
+
+      </ScrollControls>
     </Canvas>
   )
 }
