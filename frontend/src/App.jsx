c822c96637d37e03d60e57ba866ab21b6c7d86c8
Albert <adalbert123640@gmail.com>
2022-11-11 11:16:37 +0100

    added custom camera

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,44 @@
 import * as THREE from "three";
-
-import React, { useEffect } from "react";
-import { Canvas, useThree, extend } from '@react-three/fiber'
+import React, { 
+  useEffect,
+  useRef,
+} from "react";
+import { 
+  Canvas, 
+  useThree, 
+  useFrame,
+  extend, 
+} from '@react-three/fiber'
 import { 
   OrbitControls, 
   PresentationControls,
-  PerspectiveCamera
+  PerspectiveCamera,
 } from '@react-three/drei'
-
 import Office from './components/Office'
-import Office2 from './components/Office2'
+
+const deg2rad = degrees => degrees * (Math.PI / 180);
+
+function Camera(props) {
+  const ref = useRef()
+  const set = useThree((state) => state.set);
+  // This makes sure that size-related calculations are proper
+  // Every call to useThree will return this camera instead of the default camera 
+  useEffect(() => void set({ camera: ref.current }), []);
+  useFrame(() => ref.current.updateMatrixWorld());
+  return <perspectiveCamera ref={ref} {...props} />
+}
 
 export default function App() {
   return (
     <Canvas shadows={true}>
-      <PerspectiveCamera position={[10, 1, 10]} />
       <OrbitControls />
+      <Camera 
+        position={[1, 3, 0]} 
+        rotation={[deg2rad(30), 0, 0]} 
+      />
       <color attach="background" args={['#121212']} />
       <spotLight 
-        position={[3, 3, 3]} 
+        position={[1, 3, 0]} 
         angle={0.2} 
         penumbra={0.7}
         intensity={1.5}
