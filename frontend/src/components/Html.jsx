ecefda82a881809845d3e1a4ae33200121ea519f
Albert <adalbert123640@gmail.com>
2022-11-14 21:00:09 +0100

    Update Html.jsx

--- frontend/src/components/Html.jsx
+++ frontend/src/components/Html.jsx
@@ -1,76 +1,44 @@
+import { useThree } from '@react-three/fiber'
+
+function Home() {
+
+}
+
+function About() {
+
+}
+
+function Projects() {
+
+}
+
+function Tools() {
+
+}
+
+function Contact() {
+
+}
+
+function Resume() {
+
+}
+
+function AIArt() {
+
+}
+
 export default function Html() {
-    return (
-      <>
-        <h1
-        style={{
-          position: 'absolute',
-          top: '50vh',
-          left: '50vw',
-          transform: 'translateX(-50%)',
-          color: '#ee6644'
-        }}>
-          hello.
-        </h1>
-        <h1
-        style={{
-          position: 'absolute',
-          top: '100vh',
-          left: '50vw',
-          transform: 'translateX(-50%)',
-          color: '#ee6644'
-        }}>
-          hey.
-        </h1>
-        <h1
-        style={{
-          position: 'absolute',
-          top: '150vh',
-          left: '50vw',
-          transform: 'translateX(-50%)',
-          color: '#ee6644'
-        }}>
-          hi.
-        </h1>
-        <h1
-        style={{
-          position: 'absolute',
-          top: '200vh',
-          left: '50vw',
-          transform: 'translateX(-50%)',
-          color: '#ee6644'
-        }}>
-          7.
-        </h1>
-        <h1
-        style={{
-          position: 'absolute',
-          top: '250vh',
-          left: '50vw',
-          transform: 'translateX(-50%)',
-          color: '#ee6644'
-        }}>
-          8.
-        </h1>
-        <h1
-        style={{
-          position: 'absolute',
-          top: '300vh',
-          left: '50vw',
-          transform: 'translateX(-50%)',
-          color: '#ee6644'
-        }}>
-          9.
-        </h1>
-        <h1
-        style={{
-          position: 'absolute',
-          top: '350vh',
-          left: '50vw',
-          transform: 'translateX(-50%)',
-          color: '#ee6644'
-        }}>
-          10.
-        </h1>
-      </>
-    )
-  }
+  const { width: w, height: h } = useThree((state) => state.viewport)
+  return (
+    <Scroll html>
+      <Home />
+      <About />
+      <Projects />
+      <Tools />
+      <Contact />
+      <Resume />
+      <AIArt />
+    </Scroll>
+  )
+}
