caa4f9d7731168d57b78844655bbbad8d10325a3
Albert <adalbert123640@gmail.com>
2022-11-14 22:07:59 +0100

    move office canvas to office.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Canvas } from '@react-three/fiber'
-import { useGLTF, ScrollControls } from '@react-three/drei'
-import Office from './components/Office'
-import Html from './components/Html'
+import { useGLTF } from '@react-three/drei'
+import { OfficeCanvas } from './components/Office'
 import LoadingScreen from './components/Loading'
-
-function OfficeCanvas() {
-  return (
-    <Canvas shadows={true} camera={{position: [1,2,0]}}>
-      <color attach="background" args={['#000000']} />
-      <spotLight 
-        position={[1, 3, 0]} 
-        angle={0.2} 
-        penumbra={0.7}
-        intensity={1.5}
-        castShadow
-        shadow-mapSize-height={512}
-        shadow-mapSize-width={512}
-      />
-      
-      <ScrollControls pages={7}>
-        <Office />
-        <Html />
-      </ScrollControls>
-    </Canvas>
-  )
-}
 
 export default function App() {
   const [loading, setLoading] = useState(true);
