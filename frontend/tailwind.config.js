ee6917304598712b4ff977c3094ade7525a58a49
Albert <adalbert123640@gmail.com>
2022-11-16 14:04:08 +0100

    update color palette and theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,28 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'transparent': 'transparent',
+      'kimberly':'#626097',
+      'careys-pink':'#D0A7BB',
+      'cinder':'#100E1B',
+      'logan':'#ABACD0',
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    },
   },
   plugins: [],
 }
